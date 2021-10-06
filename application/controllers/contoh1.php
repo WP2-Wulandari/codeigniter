25943de124b4db09f90b667fd8d7a88e42f5510a
WP2-Wulandari <wulanshopsambi@gmail.com>
2021-10-06 10:51:44 +0700

    aplud pertama nih

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan<h1>";
+        echo"Nama saya wulandari 
+             saya tinggal di daerah sambi
+             olah raga yang saya sukai adalah 
+             lompat tali";
+    }
+}
